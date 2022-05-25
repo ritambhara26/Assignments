06897a5f47ebf34a08f60733a60a47bb8618d3a0
ritachat <ritachat@LIN59021162.corp.capgemini.com>
2022-05-25 13:18:54 +0530

    Question 3: XML to DB

---
+++ Ques3/src/main/java/com/example/ques3/config/processor/PersonItemProcessor.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.example.ques3.config.processor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.ItemProcessor;
+
+import com.example.ques3.model.Person;
+
+/**
+ * @author RITAMBHARA
+ *
+ */
+public class PersonItemProcessor implements ItemProcessor<Person, Person>{
+
+	private static final Logger log = LoggerFactory.getLogger(PersonItemProcessor.class);
+	@Override
+	public Person process(Person item) throws Exception {
+		// TODO Auto-generated method stub
+		if(item.getAge()>30) {
+			return item;
+		}
+		log.info("People are ( " + item + ")");
+		return null;
+	}
+	
+	
+
+}
