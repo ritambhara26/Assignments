52d0840dfa4fcf881227d2d93e5d5e76aa7a4e38
Ritambhara Chatterjee <89031335+ritambhara26@users.noreply.github.com>
2022-05-25 23:22:07 +0530

    Question 8: Rest Service

---
+++ Ques8_MySQL/src/main/java/com/example/ques8/model/Employee.java
@@ -0,0 +1,81 @@
+package com.example.ques8.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+ 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	int id;
+	
+	@Column(name = "name")
+	String name;
+	
+	@Column(name = "email")
+	String email;
+	
+	@Column(name = "salary")
+	int salary;
+	
+	public Employee() {
+		
+	}
+
+	public Employee(int id, String name, String email, int salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.salary = salary;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", email=" + email + ", salary=" + salary + "]";
+	}
+	
+	
+	
+}
---
+++ Ques8_MySQL/src/test/java/com/example/ques8/Ques8MySqlApplicationTests.java
@@ -0,0 +1,44 @@
+package com.example.ques8;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.ques8.controller.EmployeeController;
+import com.example.ques8.model.Employee;
+import com.example.ques8.repository.EmployeeRepository;
+
+@SpringBootTest
+class Ques8MySqlApplicationTests {
+	/*
+	 @Test void contextLoads() { }
+	 */
+
+	@Autowired
+	EmployeeController employeecontroller;
+	
+	@Mock
+	EmployeeRepository employeerepository;
+	
+	
+	
+	@Test
+	public void getUsersTest() {
+	when(employeerepository.findAll())
+	.thenReturn(Stream.of(new Employee(1,"Sam","sam@gmail.com",20000))
+	.collect(Collectors.toList()));
+	assertEquals(3, employeecontroller.getEmp().size());
+	}
+	
+	
+}
